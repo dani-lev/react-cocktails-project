42de91ae27a4ec72c2b7930cfa39e9748560f46e
Danielle Levine <dsl44d@gmail.com>
2021-10-20 19:13:02 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam sint
+        ut iusto, reiciendis, assumenda temporibus eos sed veniam impedit minima
+        non debitis sequi eveniet. Suscipit ratione pariatur laudantium qui,
+        labore perspiciatis autem nemo iure a, earum dignissimos mollitia
+        perferendis alias?
+      </p>
+    </section>
+  );
+};
+
+export default About;
